9921e2ca0e5663fcceb7a610b6c9321d6ec4a918
Elena <zim1k@mail.ru>
2022-09-17 11:32:44 +0400

    add Task01

---
+++ Task01/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+int square = num2 * num2;
+if (num1 == square)
+{
+    Console.WriteLine("Првое чило является квадратом второго");
+}
+else
+{
+    Console.WriteLine("Первое число не является квадратом второго");
+}
