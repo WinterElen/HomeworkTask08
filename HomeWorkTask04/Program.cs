8bcacd4c915ab2c419886df58c9d5e11f7ad7a49
Elena <zim1k@mail.ru>
2022-09-18 16:15:53 +0400

    Ефыл04

---
+++ HomeWorkTask04/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+int max = num1;
+if (num2 > num1) 
+if (num2 > num3)
+{
+  Console.WriteLine($"max = {num2}");
+}
+if (num3 > num2) 
+if (num3 > num1)
+{
+    Console.WriteLine($"max = {num3}"); 
+}
+if (num1 > num2)
+if (num1 > num3)
+{
+    Console.WriteLine($"max = {num1}");
+}
