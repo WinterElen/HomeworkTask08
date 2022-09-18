bb7cc7f9af22d24fdabcb2a6fb23d53f36da5de2
Elena <zim1k@mail.ru>
2022-09-18 16:19:22 +0400

    Task08

---
+++ HomeworkTask08/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int counter = 1;
+while (true);
+{
+      if (counter == counter % 2)
+
+{
+Console.Write($"{counter} ");
+ Console.Write($"{counter++} ");
+}
+if (counter > num)
+{
+    break
+}
+Console.Write($"{counter++} ");
+}
+  
+
