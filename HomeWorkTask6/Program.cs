5345ccd46758c873959acba0a426de0ccdfc1dfb
Elena <zim1k@mail.ru>
2022-09-18 14:43:13 +0400

    add Task06

---
+++ HomeWorkTask6/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num == num % 2)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
