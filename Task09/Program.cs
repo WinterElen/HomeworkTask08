40cc993b1d2c67c3772f5647ac0e88c52a167d75
Elena <zim1k@mail.ru>
2022-09-19 14:35:23 +0400

    add Task09

---
+++ Task09/Program.cs
@@ -0,0 +1,18 @@
+﻿int MaxDigit(int num)
+{
+    int firstDigit = num / 10;
+    int secondDigit = num % 10;
+    if(IsEqualDigits(firstDigit, secondDigit)) return 0;
+    return firstDigit > secondDigit ? firstDigit : secondDigit;
+}
+
+bool IsEqualDigits(int num1, int num2)
+{
+    return num1 == num2;
+}
+
+int number = new Random().Next(10, 100);
+int maxDigit = MaxDigit (number);
+
+string result = maxDigit > 0 ? maxDigit.ToString() : "Цифры равны";
+Console.WriteLine($"Наибольшая цифра числа {number} = {maxDigit}");
