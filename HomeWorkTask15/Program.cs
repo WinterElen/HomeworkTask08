91dce4dbad6c2a4b1bad1ccebad78ce3a71f44c8
Elena <zim1k@mail.ru>
2022-09-20 20:32:36 +0400

    add Task15

---
+++ HomeWorkTask15/Program.cs
@@ -0,0 +1,10 @@
+﻿
+Console.WriteLine("Введите число от 1 до 7: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+bool IsWeekEnd(int arg)
+{
+    return arg < 6;
+}
+ if (IsWeekEnd(number)) Console.WriteLine($"{number} -> Нет(");
+ else Console.WriteLine($"{number} -> Да!");
