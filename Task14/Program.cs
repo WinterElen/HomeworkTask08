70b6cda8b58b56a92c63d3330d1938262e94d501
Elena <zim1k@mail.ru>
2022-09-20 14:44:58 +0400

    add Task14

---
+++ Task14/Program.cs
@@ -0,0 +1,15 @@
+﻿// Console.WriteLine("Введите первое число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// if(number % 7 == 0 && number % 23 == 0) Console.WriteLine($"{number}, кратно");
+// else Console.WriteLine($"{number}, не кратно");
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+bool IsAliqut(int arg)
+{
+    return arg % 7 == 0 && arg % 23 == 0;
+}
+ if (IsAliqut(number)) Console.WriteLine("Да!");
+ else Console.WriteLine("Нет!");
+ 
