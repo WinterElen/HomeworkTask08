d1678f42d221d110327b86a6d2f2bf29c3de1433
Elena <zim1k@mail.ru>
2022-09-20 14:50:10 +0400

    add Task16

---
+++ Task16/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+bool IsSquare(int num1, int num2)
+{
+    return num1 == num2 * num2 || num2 == num1 * num1;
+}
+ if (IsSquare(number1, number2)) Console.WriteLine($"{number1}, {number2} => Да!");
+ else Console.WriteLine($"{number1}, {number2} => Нет!");
+ 
