612d9a3500cf7837b06cfce676a8f34613996596
Elena <zim1k@mail.ru>
2022-09-20 14:16:31 +0400

    add Task12

---
+++ Task12/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+bool IsSquare(int arg1, int arg2)
+{
+    return arg1 % arg2 == 0;
+}
+ if (IsSquare(num1, num2)) Console.WriteLine($"{num1} кратно{num2}");
+ else Console.WriteLine($"{num1} не кратно {num2} Остаток {num1 % num2}");
+ 
