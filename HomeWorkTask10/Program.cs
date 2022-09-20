a29e71fc449228caa8f6059db814bf47e009733c
Elena <zim1k@mail.ru>
2022-09-20 19:57:21 +0400

    add Task10

---
+++ HomeWorkTask10/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int ShowSecondDigit (int num)
+{
+    int secondDigit = num / 10;
+    return secondDigit % 10;
+}
+int result = ShowSecondDigit(number);
+Console.WriteLine($"{result}");
