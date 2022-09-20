a5838f2d9c8257ea7cc0052b2a75e02952eda94c
Elena <zim1k@mail.ru>
2022-09-20 13:00:33 +0400

    add Task11

---
+++ Task11/Program.cs
@@ -0,0 +1,21 @@
+﻿
+int number = new Random().Next(100, 1000);
+int RemoveSecondDigit (int num)
+{
+    int thirdDigit = num % 10;
+    int firstDigit = num / 100;
+    return firstDigit * 10 + thirdDigit;
+}
+int result = RemoveSecondDigit(number);
+Console.WriteLine($"Итоговое число от {number} => {result}");
+
+
+//int FirstThird(int res)
+//{
+   // int firstDigit = num / 100;
+    //int thirdDigit = num % 10;
+    //return firstDigit * 10 + thirdDigit;
+//}
+//int number = newRandom().Next(100, 1000);
+//int result = FirstThird(number);
+//Console.Write($"{number}");
